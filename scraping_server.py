dd0c50cde154e07aae73fece5f31bd822eca3f73
Cristian Ninicu <cristian.ninicu@structuralmanagement.ro>
2019-12-01 01:06:39 +0200

    ds

--- scraping_server.py
+++ scraping_server.py
@@ -1,5 +1,17 @@
-from os import environ
+# from os import environ
+# from flask import Flask
+#
+# app = Flask(__name__)
+# app.run(environ.get('PORT'))
+import os
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+
+@app.route("/")
+def hello():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
