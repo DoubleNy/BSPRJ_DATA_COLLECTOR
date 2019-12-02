a4e4d8c7b183386bc09e7e64e98edc09aac54ceb
Cristian Ninicu <cristian.ninicu@structuralmanagement.ro>
2019-12-02 20:20:06 +0200

    WIP

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 TRACK_WORDS = ['Facebook', 'google', 'amazon', 'elon musk']
-TABLE_NAME = "DATA_COLLECTION"
+TABLE_NAME = "\"DATA_COLLECTION\""
 TABLE_ATTRIBUTES = "id_str varchar(255), created_at date, text varchar(255), \
             polarity integer, subjectivity integer, user_created_at varchar(255), user_location varchar(255), \
             user_description varchar(255), user_followers_count integer, longitude double, latitude double, \
