7792d072a2890448ba935149c24ba7faa34c8b37
Cristian Ninicu <cristian.ninicu@structuralmanagement.ro>
2019-12-02 19:58:02 +0200

    WIP

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 TRACK_WORDS = ['Facebook', 'google', 'amazon', 'elon musk']
 TABLE_NAME = "Facebook"
-TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
-            polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
-            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
-            retweet_count INT, favorite_count INT"
+TABLE_ATTRIBUTES = "id_str varchar(255), created_at date, text varchar(255), \
+            polarity integer, subjectivity integer, user_created_at varchar(255), user_location varchar(255), \
+            user_description varchar(255), user_followers_count integer, longitude double, latitude double, \
+            retweet_count integer, favorite_count integer"
